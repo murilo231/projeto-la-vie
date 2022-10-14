fd5b1d362a652621d8b86bbfd8b9c25f7ecdb1a9
Hudson Menezes <hudson.bm23@gmail.com>
2022-10-14 15:27:50 -0300

    Pacotes instalados e arquivos iniciais criados

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const { application } = require('express')
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+app.use(routes)
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
